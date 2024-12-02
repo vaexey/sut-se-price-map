718e2a0c9910829c9d5b3faac3b8f035b7adcb59
sebasion9 <kansebakk@gmail.com>
2024-12-02 21:45:56 +0100

    init backend

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
-# Price Map  
-*(name to be discussed)*  
+# price-map  
 **Web app made to compare prices of different products across multiple stores.**  
+
+
+# Install
+
+Install version 1.23.3 or newer of [go](https://go.dev/doc/install).
+
+Make sure **go** is installed correctly.
+
+```
+go version
+```
+
+To run backend
+
+```
+go run back/main.go
+```
---
+++ back/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+const ADDR = ":6969"
+
+func hello(w http.ResponseWriter, req *http.Request) {
+    id := req.PathValue("id")
+    param := req.URL.Query().Get("param")
+
+    data := map[string]any {
+            "message" : "hello",
+            "error" : nil,
+            "id": id,
+            "query-param": param,
+    }
+
+    w.Header().Add("Content-Type", "application/json")
+    w.WriteHeader(http.StatusOK) // 200
+
+    json.NewEncoder(w).Encode(data)
+}
+
+func main() {
+
+    fmt.Printf("listening on: %s\n", ADDR)
+    http.HandleFunc("GET /hello/{id}", hello);
+    http.HandleFunc("/", hello);
+    http.ListenAndServe(ADDR, nil)
+}
+
