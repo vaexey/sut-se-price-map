04556c9a8da353fb3ca654eca3ef25543b3375e8
sebasion9 <kansebakk@gmail.com>
2025-01-28 21:43:16 +0100

    add retrieving include/exclude filters from params

--- back/db/contrib.go
+++ back/db/contrib.go
@@ -4,14 +4,8 @@ import (
 	"back/model"
 )
 
-// func (cs *contribService) SelectAll() ([]model.User, error) {
-// 	var user []model.User
-// 	result := uh.Db.Find(&user)
-// 	return user, result.Error
-// }
-
 func (cs *contribService) SelectById(id uint) (model.Contrib, error) {
-	var contrib model.Contrib
-	err := cs.Db.Preload("Product").Preload("Store").Preload("Author").First(&contrib, id).Error
+    var contrib model.Contrib
+    err := cs.Db.Preload("Product").Preload("Store").Preload("Author").First(&contrib, id).Error
     return contrib, err
 }
