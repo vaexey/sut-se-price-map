3f73839dd5817070a5d0dae2991038874572b42b
BartekWons <bartek.wons@gmail.com>
2025-02-01 18:36:59 +0100

    fix error handling

--- back/api/product.go
+++ back/api/product.go
@@ -2,9 +2,11 @@ package api
 
 import (
 	"back/model"
+	"errors"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 func (a *Api) Products(c *gin.Context) {
@@ -12,8 +14,15 @@ func (a *Api) Products(c *gin.Context) {
 	var err error
 	products, err = a.Db.Product.SelectAll()
 	if err != nil {
-		c.JSON(http.StatusNotFound, gin.H {
-			"message": "Record not found",
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(http.StatusNotFound, gin.H {
+				"message": "Record not found",
+			})
+			c.Abort()
+			return
+		}
+		c.JSON(http.StatusServiceUnavailable, gin.H {
+			"message": "Service failure",
 		})
 		return
 	}
