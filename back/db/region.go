389e2d4d9f7a5cf9bdf0411d801357ee5c18dc93
BartekWons <bartek.wons@gmail.com>
2025-02-01 13:30:57 +0100

    add function which returns Ids of all children's of region

--- back/db/region.go
+++ back/db/region.go
@@ -33,3 +33,30 @@ func (rh *regionService) CountParents(id uint) (int, error) {
 	}
 	return count, nil
 }
+
+func (rh *regionService) SelectChildren(parentId uint) ([]uint, error) {
+	var regions []model.Region
+
+	result := rh.Db.Where("parent = ?", parentId).Find(&regions)
+	if result.Error != nil {
+		return nil, result.Error
+	}
+
+	var childrenIDs []uint
+
+	// Pobieramy ID wszystkich bezpośrednich dzieci
+	for _, region := range regions {
+		childrenIDs = append(childrenIDs, region.Id)
+
+		// Rekurencyjne pobieranie ID dzieci tego regionu
+		subChildren, err := rh.SelectChildren(region.Id)
+		if err != nil {
+			return nil, err
+		}
+
+		// Dodajemy ID zagnieżdżonych dzieci do listy
+		childrenIDs = append(childrenIDs, subChildren...)
+	}
+
+	return childrenIDs, nil
+}
