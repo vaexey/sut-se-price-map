d1b4f12fe99293de56d273ce9adb18704e784737
vaexey <vaexey@gmail.com>
2025-02-03 17:16:54 +0100

    fix region model not having id json tag

--- back/model/region.go
+++ back/model/region.go
@@ -1,7 +1,7 @@
 package model
 
 type Region struct {
-	Id          uint   `gorm:"unique;primaryKey;autoIncrement"`
+	Id          uint   `gorm:"unique;primaryKey;autoIncrement" json:"id"`
 	Parent      uint   `json:"parent"`
 	Name        string `json:"name"`
 	ParentCount uint   `json:"parentCount"`
